0ead8323a8294215b9cc2e023a292b4d0261ea6b
ninja03 <69106571+ninja03@users.noreply.github.com>
2021-07-12 01:37:31 +0900

    ts

--- server.ts
+++ server.ts
@@ -1,18 +1,19 @@
 // https://fukuno.jig.jp/2943
-import { Server } from "https://code4sabae.github.io/js/Server1.js";
-import { jsonfs } from "https://code4sabae.github.io/js/jsonfs.js";
+import { Server } from "https://code4sabae.github.io/js/Server1.js"
+import { jsonfs } from "https://code4sabae.github.io/js/jsonfs.js"
 
 class DB {
-  constructor() {
-    this.timeline = [];
-  }
+  timeline: Array<Post> = []
 }
 
 class Post {
-  constructor(id, url) {
-    this.id = id;
-    this.url = url;
-    this.good = 0;
+  id: number
+  url: string
+  good = 0
+
+  constructor(id: number, url: string) {
+    this.id = id
+    this.url = url
   }
 }
 
@@ -22,56 +23,56 @@ class MyServer extends Server {
   // pathにはアクセスしたURLの「/api/○○」が入っていて
   // reqにはPOSTで送るJSONが入っています
   // resp変数にフロントエンドに返したい内容をセットします
-  api(path, req) {
-    let db; // データベースの内容（db.json)
+  api(path: string, req: any) {
+    let db: DB  // データベースの内容（db.json)
     try {
       // DBファイルの読み込み
       // ※誰かがこのapi関数にアクセスしているときは、ほかのユーザはその処理が終わるのを待つのでdb.jsonで不整合は生じない
-      db = jsonfs.read("db.json");
+      db = jsonfs.read("db.json")
     } catch (e) {
       // DBファイルがなければ空データの作成
-      db = new DB();
+      db = new DB()
     }
     let resp = {} // フロントエンドに返すJSON
-    if (path === "/api/timeline") {
+    if (path == "/api/timeline") {
       // タイムラインをそのまま返す
-      resp = db.timeline;
-    } else if (path === "/api/post") {
+      resp = db.timeline
+    } else if (path == "/api/post") {
       // タイムラインに投稿する
       // 画像ファイル自体は「/data/」のAPIで送られていて
       // 画像のURLがreq.urlで送られてくるのでそれを保存する
-      let nextid;
-      if (db.timeline.length === 0) {
+      let nextid
+      if (db.timeline.length == 0) {
         // 初投稿はIDを1にする
-        nextid = 1;
+        nextid = 1
       } else {
         // 2つめ投稿以降は最新のID足す1
-        nextid = db.timeline[0].id + 1;
+        nextid = db.timeline[0].id + 1
       }
-      const newpost = new Post(nextid, req.url);
+      const newpost = new Post(nextid, req.url)
       // timeline配列の先頭に追加する
-      db.timeline.unshift(newpost);
-    } else if (path === "/api/good") {
+      db.timeline.unshift(newpost)
+    } else if (path == "/api/good") {
       // 写真にいいねをする
       // タイムラインからIDで探して、いいねを1増やす
       for (let i = 0; i < db.timeline.length; i++) {
         if (db.timeline[i].id === req.id) {
-          db.timeline[i].good++;
-          break;
+          db.timeline[i].good++
+          break
         }
       }
-    } else if (path === "/api/trend") {
+    } else if (path == "/api/trend") {
       // いいねの多い順に並び替えて返す
       // db.timeline.sortにするとDBの内容まで変わってしまうので注意！
       // trend変数にコピー(slice)してからソートする
-      const trend = db.timeline.slice();
-      trend.sort((a, b) => b.good - a.good);
-      resp = trend;
+      const trend = db.timeline.slice()
+      trend.sort((a, b) => b.good - a.good)
+      resp = trend
     }
     // DBの保存
-    jsonfs.write("db.json", db);
-    return resp;
+    jsonfs.write("db.json", db)
+    return resp
   }
 }
 
-new MyServer(80);
+new MyServer(80)
