544ee462d5941ca00869f9effa89d44aa58c95d4
ninja03 <69106571+ninja03@users.noreply.github.com>
2021-07-12 19:50:47 +0900

    ts化

--- front.ts
+++ front.ts
@@ -2,28 +2,36 @@ import Alpine from "https://taisukef.github.io/alpine_es/es/index.js";
 import { fetchJSON } from "https://code4sabae.github.io/js/fetchJSON.js";
 import { ImageUploader } from "https://code4sabae.github.io/js/ImageUploader.js";
 
+interface Post {
+  id: number;
+  url: string;
+  good: number
+}
+
 class Client {
-  async reload(type) {
+  timeline: Array<Post> = [];
+
+  async reload(type: string) {
     this.timeline = await fetchJSON("/api/" + type, {});
   }
-  async good(id) {
+  async good(id: number) {
     await fetchJSON("/api/good", {id: id});
   }
-  upload(file, callback) {
-    const uploader = new ImageUploader("/data/")
+  upload(file: string, callback: any) {
+    const uploader = new ImageUploader("/data/");
     // 最大幅1200px、最大ファイルサイズ1メガバイト
-    uploader.setFile(file, 1200, 1024 * 1024)
-    uploader.onload = async (url) => {
+    uploader.setFile(file, 1200, 1024 * 1024);
+    (uploader as any).onload = async (url: string) => {
         await fetchJSON("/api/post", {url: url});
         callback();
     }
   }
 }
 
-const client = new Client()
+const client = new Client();
 
-window.main = () => ({
-  timeline: [],
+(window as any).main = () => ({
+  timeline: [] as Array<Post>,
   type: "timeline",
   async init() {
     await this.reload();
@@ -32,7 +40,7 @@ window.main = () => ({
     await client.reload(this.type)
     this.timeline = client.timeline;
   },
-  upload(e) {
+  upload(e: any) {
     for (let i = 0; i < e.files.length; i++) {
       client.upload(e.files[i], () => this.reload());
     }
@@ -41,7 +49,7 @@ window.main = () => ({
     this.type = this.type == "timeline" ? "trend" : "timeline";
     await this.reload();
   },
-  async good(id) {
+  async good(id: number) {
     await client.good(id);
     await this.reload();
   }
--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-deno run -A --watch --unstable server.ts
+start deno run -A --watch --unstable server.ts
+start deno bundle --watch front.ts static/bundle.js
